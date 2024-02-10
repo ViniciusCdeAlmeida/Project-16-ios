2b46123601ec5b1dde09e4971224fdf9a953f2b0
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-10 18:17:05 -0300

    Initial Commit

---
+++ Rich App/Rich_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rich_AppApp.swift
+//  Rich App
+//
+//  Created by Apple Multiplier on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Rich_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

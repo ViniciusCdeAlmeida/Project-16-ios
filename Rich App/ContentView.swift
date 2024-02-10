832711060702c0458e1a9ce584076f9439da7190
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-10 18:35:55 -0300

    feat: using swiftui

--- Rich App/ContentView.swift
+++ Rich App/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        ZStack{
+            Color(.systemTeal).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diamond")
+                    .resizable(resizingMode: .stretch).aspectRatio(contentMode: .fit).frame(width: 200.0, height: 200.0,alignment: .center)
+                Text("Rich App").font(.system(size: 40)).fontWeight(.bold).foregroundColor(Color.white)
+            }
         .padding()
+        }
     }
 }
 
